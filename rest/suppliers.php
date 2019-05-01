4a4e6e138aad84e61c09cdef1e2ba3d2a8f27007
Loc Vuu <loc@vuudoo.com>
2019-05-01 07:36:47 -0700

    Adding rest calls to build react app.

---
+++ rest/suppliers.php
@@ -0,0 +1,30 @@
+<?php include "../connection.php"; /** calling of connection.php that has the connection code **/ 
+
+    // products array
+    $suppliers=array();
+    $suppliers=array();
+
+    $result = $mysqli->query("SELECT idSupplier, supplier, type, contactName, email, phone, comment
+                    FROM supplier ORDER BY supplier ASC");
+    
+    while($data = $result->fetch_object()){
+     //   extract($data);
+
+        $supplier=array(
+            "id" => $data->idSupplier,
+            "supplier" => $data->supplier,
+            "type" => $data->type,
+            "contactName" => $data->contactName,
+            "email" => $data->email,
+            "phone" => $data->phone,
+            "comment" => $data->comment,
+        );
+
+//        echo $data->supplier;
+
+        array_push($suppliers, $supplier);
+
+    }
+
+    echo json_encode($suppliers);
+  
