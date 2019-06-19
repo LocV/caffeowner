ebe42cdd3b3d9816044b486db2591f66ec4dc53c
Loc Vuu <loc@vuudoo.com>
2019-06-18 19:18:54 -0700

    Adding rest service for the shopping list and list items

--- rest/shoppingList.php
+++ rest/shoppingList.php
@@ -2,24 +2,74 @@
 
     // products array
     $ShoppingLists=array();
+    $ListItems=array();
 
-    $result = $mysqli->query("SELECT id, dateCreated, name, description, status FROM ShoppingList
+    $shoppingListId=$_GET['listId'];
+
+    if ($shoppingListId == '')
+    {
+
+        $result = $mysqli->query("SELECT id, dateCreated, name, description, status FROM ShoppingList
                                                       ORDER BY dateCreated DESC
                                                       LIMIT 3");
     
-    while($data = $result->fetch_object()){
-     //   extract($data);
+        while($data = $result->fetch_object()){
+         //   extract($data);
 
-        $ShoppingList=array(
-            "id" => $data->id,
-            "name" => $data->name,
-            "description" => $data->description,
-            "status" => $data->status,
-        );
+            $ShoppingList=array(
+                "id" => $data->id,
+                "name" => $data->name,
+                "description" => $data->description,
+                "status" => $data->status,
+            );
 
-        array_push($ShoppingLists, $ShoppingList);
+            array_push($ShoppingLists, $ShoppingList);
+
+        }
+
+        echo json_encode($ShoppingLists);
+
+    }else{
+
+        $result = $mysqli->query("SELECT ShoppingList_Item.`id`, Item.idItem, ShoppingList_Item.status, urgency, Item.`item`, ShoppingList_Item.`quantity`
+            FROM `Item`, `ShoppingList_Item`
+            WHERE ShoppingList_Item.`idShoppingList` = '$shoppingListId' 
+            AND Item.`idItem` = ShoppingList_Item.`idItem`
+            order by ShoppingList_Item.status ASC, Item.`item`");
+
+          while($data = $result->fetch_object())
+          {
+            $products = $mysqli->query("SELECT price, supplier 
+                FROM `Product`, `Supplier` 
+                WHERE idItem = '$data->idItem'
+                AND `product`.`idSupplier`= `Supplier`.`idSupplier`");
+                
+                $product = $products->fetch_object();
+
+                $ListItem=array(
+                    "item" => $data->item,
+                    "idItem" => $data->idItem,
+                    "quantity" => $data->quantity,
+                    "price" => $product->price,
+                    "supplier" => $product->supplier
+                );
+
+                array_push($ListItems, $ListItem);
+            }
+
+        echo json_encode($ListItems);
 
     }
 
-    echo json_encode($ShoppingLists);
+
+
+
+
+
+
+
+
+
+
+
   
